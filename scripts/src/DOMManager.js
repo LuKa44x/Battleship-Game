1347ff53873f427abc084ee5543e7f531665588f
LuKa44x <lucavicari44@gmail.com>
2025-09-19 11:17:38 +0200

    feat: add AI checkbox control to enable/disable Player 2 input
    - Added `AICheckboxControl` function to check checkbox state
    - Added `AICheckboxChecked` function to handle `change` event:
      - Enable/disable `#player2Name` input
      - Clear input value when disabled
    - Updated logic to assign Player 2 or AI name based on checkbox state
    - Added styles for disabled input (dark background, gray text, line-through).

--- scripts/src/DOMManager.js
+++ scripts/src/DOMManager.js
@@ -15,12 +15,19 @@ export function createBoard(player) {
 
 export function startGame(player1, player2) {
   const player1Input = document.querySelector('#player1Name');
-  const player2Input = document.querySelector('#player2Name');
-  removeBlur();
   if (!player1Input.value) player1Input.value = 'Player 1';
-  if (!player2Input.value) player2Input.value = 'Player 2';
   player1.name = player1Input.value;
-  player2.name = player2Input.value;
+  console.log(AICheckboxControl());
+  if (!AICheckboxControl()) {
+    const player2Input = document.querySelector('#player2Name');
+    if (!player2Input.value) player2Input.value = 'Player 2';
+    player2.name = player2Input.value;
+  } else {
+    player2.isAi = true;
+    player2.name = 'AI';
+  }
+  removeBlur();
+
   createBoard(document.querySelector('.player1-gameboard-container'));
   createBoard(document.querySelector('.player2-gameboard-container'));
 }
@@ -29,4 +36,21 @@ function removeBlur() {
   document.querySelector('.modal-overlay').style.pointerEvents = 'none';
   document.querySelector('.modal-overlay').style.display = 'none';
   document.body.classList.add('no-blur');
+}
+
+function AICheckboxControl() {
+  const checkbox = document.querySelector('.ai-checkbox');
+  if (checkbox.checked) return true;
+  else return false;
+}
+//check quando clicco checkbox disable input2, se clicco di nuovo abilita
+export function AICheckboxChecked() {
+  const checkbox = document.querySelector('.ai-checkbox');
+  const player2Input = document.querySelector('#player2Name');
+  checkbox.addEventListener('change', (e) => {
+    player2Input.disabled = e.target.checked;
+    if (e.target.checked) {
+      player2Input.value = ''; // Svuota quando disabilitato
+    }
+  });
 }
